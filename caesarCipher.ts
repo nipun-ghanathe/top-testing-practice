7e86cd2dde0c7695e5543af488ab6393d3d04fb1
nipun <nipuncod@gmail.com>
2026-03-26 16:18:55 +0530

    refactor caesarCipher to smaller functions

--- caesarCipher.ts
+++ caesarCipher.ts
@@ -1,21 +1,17 @@
+function getShiftedCharCode(charCode: number, shift: number) {
+  const base =
+    charCode >= 65 && charCode <= 90
+      ? 65
+      : charCode >= 97 && charCode <= 122
+        ? 97
+        : null
+  return base !== null ? ((charCode - base + shift + 26) % 26) + base : charCode
+}
+
 export function caesarCipher(str: string, shift: number) {
-  const cipherCodes: number[] = []
-
-  for (let char of str) {
-    const charCode = char.charCodeAt(0)
-
-    const base =
-      charCode >= 65 && charCode <= 90
-        ? 65
-        : charCode >= 97 && charCode <= 122
-          ? 97
-          : null
-
-    const newCharCode =
-      base !== null ? ((charCode - base + shift + 26) % 26) + base : charCode
-
-    cipherCodes.push(newCharCode)
-  }
-
-  return String.fromCharCode(...cipherCodes)
+  return String.fromCharCode(
+    ...Array.from(str).map((char) =>
+      getShiftedCharCode(char.charCodeAt(0), shift),
+    ),
+  )
 }
