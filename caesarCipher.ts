8b44061516e1584ec5991f50d559eec6a901471a
nipun <nipuncod@gmail.com>
2026-03-26 16:04:53 +0530

    reduce branching in caesarCipher - combine lowercase and uppercase logic

--- caesarCipher.ts
+++ caesarCipher.ts
@@ -3,17 +3,16 @@ export function caesarCipher(str: string, shift: number) {
 
   for (let char of str) {
     const charCode = char.charCodeAt(0)
-    let newCharCode: number
 
-    if (charCode >= 65 && charCode <= 90) {
-      const base = 65
-      newCharCode = ((charCode - base + shift + 26) % 26) + base
-    } else if (charCode >= 97 && charCode <= 122) {
-      const base = 97
-      newCharCode = ((charCode - base + shift + 26) % 26) + base
-    } else {
-      newCharCode = charCode
-    }
+    const base =
+      charCode >= 65 && charCode <= 90
+        ? 65
+        : charCode >= 97 && charCode <= 122
+          ? 97
+          : null
+
+    const newCharCode =
+      base !== null ? ((charCode - base + shift + 26) % 26) + base : charCode
 
     cipherCodes.push(newCharCode)
   }
